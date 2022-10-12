3f77416f93b83558db8c5e43e494709a722348ee
Viet Tran <vtran86@uwo.ca>
2022-10-12 15:30:43 -0400

    setting up

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+export default function Nav(){
+    return(
+        <div className="nav-bar">
+            <h3 className="name">Computer System Design Tools</h3>
+            <div>
+                <i class="uil uil-sun"></i>
+                <i class="uil uil-apps" id="toggle-nav"></i>
+            </div>
+        </div>
+    )
+}
