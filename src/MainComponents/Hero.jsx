27f3d5923043a827878e08d1fa189531f8eccf5b
NDSern <potarau123@gmail.com>
2022-10-14 00:53:59 +0300

    Hero sections with some child elements

--- src/MainComponents/Hero.jsx
+++ src/MainComponents/Hero.jsx
@@ -1,5 +1,16 @@
-export default function Hero(){
-    return(
-        <div></div>
-    )
-}
+import React from "react";
+
+const Hero = () => {
+  return (
+    <>
+      <div className="hero">
+        <div>Easy time designing Computer System</div>
+        <h2>BIG ASS MOTO HERE</h2>
+        <p>Have fun!!</p>
+        <button>Random button that may have some use</button>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,13 @@
-export default function Nav(){
-    return(
-        <div className="nav-bar">
-            <h3 className="name">Computer System Design Tools</h3>
-            <div>
-                <i class="uil uil-sun"></i>
-                <i class="uil uil-apps" id="toggle-nav"></i>
-            </div>
+export default function Nav() {
+  return (
+    <>
+      <div className="nav-bar">
+        <h3 className="name">Computer System Design Tools</h3>
+        <div>
+          <i className="uil uil-sun"></i>
+          <i className="uil uil-apps" id="toggle-nav"></i>
         </div>
-    )
+      </div>
+    </>
+  );
 }
