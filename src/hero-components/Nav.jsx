98abcd72a9f16c01ab7ba56578f4b3b67c0d211f
Viet Tran <vtran86@uwo.ca>
2022-11-04 12:11:48 -0400

    animated hero

--- src/hero-components/Nav.jsx
+++ src/hero-components/Nav.jsx
@@ -1,21 +1,23 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
+import { useInView } from "react-intersection-observer";
 
 export default function Nav() {
+  const { ref: navRef, inView: navVisibile, entry2 } = useInView();
   return (
-    <>
-      <div className="nav-bar">
-        <div className="nav-bar-left">
+    <div>
+      <div ref={navRef} className="nav-bar">
+        <div className={`nav-bar-left text-hidden ${navVisibile ? 'text-show' : ''}`}>
           <FontAwesomeIcon icon={faLaptopCode}></FontAwesomeIcon>
           <h3 className="name">Computer System Design Tools</h3>
         </div>
-        <div className="nav-bar-right">
+        <div className={`nav-bar-right text-hidden ${navVisibile ? 'text-show' : ''}`}>
           <a href="">Home</a>
           <a>Info</a>
           <a>Contact</a>
           <a>About us</a>
         </div>
       </div>
-    </>
+    </div>
   );
 }
