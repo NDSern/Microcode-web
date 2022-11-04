abe36c5e152d9a4ff320824d9a2e5c5ca7cde4f3
Viet Tran <vtran86@uwo.ca>
2022-11-04 10:37:57 -0400

    hero general styling

--- src/hero-components/Hero.jsx
+++ src/hero-components/Hero.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Nav from "./Nav";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowAltCircleRight } from "@fortawesome/free-regular-svg-icons";
 
 const Hero = () => {
   return (
@@ -7,10 +9,17 @@ const Hero = () => {
       <div className="hero">
         <Nav />
         <div className="hero-main">
-            <div>Easy time designing Computer System</div>
-            <h2>BIG ASS MOTO HERE</h2>
-            <p>Have fun!!</p>
-            <button>Random button that may have some use</button>
+          <div className="hero-main-text">
+              <h1>Computer System Design Toolkit</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque fuga necessitatibus iste, at illo reprehenderit animi, rem explicabo saepe fugit nulla mollitia nisi dicta beatae consectetur similique. Repellat, exercitationem consectetur!</p>
+              <button>
+                <p>Ramdom text</p>
+                <FontAwesomeIcon icon={faArrowAltCircleRight} ></FontAwesomeIcon>
+              </button>
+          </div>
+          <div className="hero-main-animation">
+            <video src="src\img\ezgif-4-903b79f303.mp4" autoPlay loop muted playsInline></video>
+          </div>
         </div>
       </div>
     </>
