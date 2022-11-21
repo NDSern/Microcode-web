0a24b8b214cbfa51c1f07a388385f12e551dd10c
NDSern <potarau123@gmail.com>
2022-11-21 16:18:01 +0200

    More polished moving things

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,34 +3,53 @@ import { useState } from "react";
 
 const Main = () => {
   const [mousePos, setMousePos] = useState({});
-  let r = document.querySelector(':root')
+  let rootVariables = document.querySelector(":root");
 
   const handleMouseMove = (event) => {
     setMousePos({
       x: event.pageX - event.target.offsetLeft,
-      y: event.pageY - event.target.offsetTop
+      y: event.pageY - event.target.offsetTop,
       // no idea why its 500 but it work
     });
-    
-    r.style.setProperty("--mouse-x", `${mousePos.x}px`);
-    r.style.setProperty("--mouse-y", `${mousePos.y}px`);
-    console.log(event.target.offsetTop, " / ")
-    console.log(mousePos)
+
+    rootVariables.style.setProperty("--mouse-x", `${mousePos.x}px`);
+    rootVariables.style.setProperty("--mouse-y", `${mousePos.y}px`);
   };
 
   let form = (
     <main>
-        <div className="main-text">
-            <h2>What we offer</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque perferendis aliquam sunt laudantium facilis et quas labore modi! Possimus excepturi sit impedit cum ab cupiditate dolorum corrupti. Eligendi, tenetur libero alias laborum voluptate id aperiam maxime? Autem, consequuntur quia ipsam dolorem, quod id incidunt, exercitationem aperiam quae quo at officia sed dolorum velit et enim ratione? Harum illum explicabo excepturi.</p>
-        </div>
+      <div className="main-text">
+        <h2>What we offer</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+          perferendis aliquam sunt laudantium facilis et quas labore modi!
+          Possimus excepturi sit impedit cum ab cupiditate dolorum corrupti.
+          Eligendi, tenetur libero alias laborum voluptate id aperiam maxime?
+          Autem, consequuntur quia ipsam dolorem, quod id incidunt,
+          exercitationem aperiam quae quo at officia sed dolorum velit et enim
+          ratione? Harum illum explicabo excepturi.
+        </p>
+      </div>
       <div className="tabs">
-        <div id="tab1" className="tab" onMouseMove={handleMouseMove}></div>
-        <div id="tab2" className="tab" onMouseMove={handleMouseMove}></div>
-        <div id="tab3" className="tab" onMouseMove={handleMouseMove}></div>
-        <div id="tab4" className="tab" onMouseMove={handleMouseMove}></div>
-        <div id="tab5" className="tab" onMouseMove={handleMouseMove}></div>
-        <div id="tab6" className="tab" onMouseMove={handleMouseMove}></div>
+        <div id="tab1" className="tab" onMouseMove={handleMouseMove}>
+          <div className="tab-content">
+          </div>
+        </div>
+        <div id="tab2" className="tab" onMouseMove={handleMouseMove}>
+          <div className="tab-content"></div>
+        </div>
+        <div id="tab3" className="tab" onMouseMove={handleMouseMove}>
+          <div className="tab-content"></div>
+        </div>
+        <div id="tab4" className="tab" onMouseMove={handleMouseMove}>
+          <div className="tab-content"></div>
+        </div>
+        <div id="tab5" className="tab" onMouseMove={handleMouseMove}>
+          <div className="tab-content"></div>
+        </div>
+        <div id="tab6" className="tab" onMouseMove={handleMouseMove}>
+          <div className="tab-content"></div>
+        </div>
       </div>
     </main>
   );
