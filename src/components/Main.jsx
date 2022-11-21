7e372c85c114ce39e6b287e646bd11511236c7fe
Viet Tran <vtran86@uwo.ca>
2022-11-21 06:54:19 -0500

    additional styiling for main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -20,10 +20,17 @@ const Main = () => {
 
   let form = (
     <main>
+        <div className="main-text">
+            <h2>What we offer</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque perferendis aliquam sunt laudantium facilis et quas labore modi! Possimus excepturi sit impedit cum ab cupiditate dolorum corrupti. Eligendi, tenetur libero alias laborum voluptate id aperiam maxime? Autem, consequuntur quia ipsam dolorem, quod id incidunt, exercitationem aperiam quae quo at officia sed dolorum velit et enim ratione? Harum illum explicabo excepturi.</p>
+        </div>
       <div className="tabs">
         <div id="tab1" className="tab" onMouseMove={handleMouseMove}></div>
         <div id="tab2" className="tab" onMouseMove={handleMouseMove}></div>
         <div id="tab3" className="tab" onMouseMove={handleMouseMove}></div>
+        <div id="tab4" className="tab" onMouseMove={handleMouseMove}></div>
+        <div id="tab5" className="tab" onMouseMove={handleMouseMove}></div>
+        <div id="tab6" className="tab" onMouseMove={handleMouseMove}></div>
       </div>
     </main>
   );
